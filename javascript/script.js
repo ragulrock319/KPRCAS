e2998bb3e0ead2e94b08aa3617a86a51bfe3136e
luffy <ragulrock319@gmail.com>
2025-06-14 15:50:22 +0530

    commit message

---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+
+var a="10";
+var b="20";
+console.log
+function sum(a,d){
+    console.log(a+b);
+}
+sum(10+20)
+
+var sum =()=> {
+    var a="10";
+    var b="20";
+    console.log(a+b);   
+}
+sum();
+let arr =[10,20,30,40];
+let arr2 =[50,60,70];
+console.log(arr);re
+console.log(arr2);
+
+var marks =[85, 90, 78, 92];
+var [m1, m2, m3, m4] = marks;
+console.log(m1);
+console.log(m2);
+console.log(m3);
+console.log(m4);
